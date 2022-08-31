e795f6373ec8e4a695c709ba467dad1ebd29eacb
Elvin.Memmedov <Elvin.Memmedov@abc-telecom.az>
2022-08-31 14:14:19 +0400

    'Initial'

--- resources/views/components/tour-types.blade.php
+++ resources/views/components/tour-types.blade.php
@@ -1,4 +1,5 @@
 @foreach($tourTypes as $t)
+
     <div class="border-b border-solid flex gap-2 p-1 hover:bg-[#8A9EAE]">
         <img src="{{$t->getFirstMediaUrl('main')}}" alt="" style="width: 24px; height: 24px">
         <a href="">{{$t->name}}</a>
